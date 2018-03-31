24b81cb681fe02d721791e7ceb373d951bc285d8
Ian Torres <iantorres@outlook.com>
2018-03-31 12:39:53 -0300

    Security increased

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Http\Requests\Admin\Users\IndexRequest;
+use App\Http\Requests\Admin\Users\ShowRequest;
 
 class UsersController extends Controller
 {
@@ -16,14 +18,14 @@ class UsersController extends Controller
 		$this->users = $users;
 	}
 
-    public function index()
+    public function index(IndexRequest $request)
     {
     	$users = $this->users->all();
 
     	return view('admin.users.index')->with('users', $users);
     }
 
-    public function show(User $user)
+    public function show(ShowRequest $request, User $user)
     {
     	return view('admin.users.show')->with('user', $user);
     }
